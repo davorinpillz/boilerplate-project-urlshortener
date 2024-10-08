1e81d79c03727037c6808e3cfbd3a7ce9059e2a3
Igor Mrtvac <ddp1329@gmail.com>
2024-10-08 10:43:20 +0000

    lashdglak

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(cors());
 
@@ -18,6 +20,20 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+let count = 0
+app.post('/api/shorturl', function(req, res) {
+
+  
+  const url = req.body.url
+  count = count + 1
+  res.set('short_url', 'test')
+  console.log(req.body, count)
+
+  res.json({"original_url": req.body.url})
+})
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
